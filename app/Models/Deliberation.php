db86382f4d39544021619235e703302a480025f5
Biaou Laurette <biaoulaurie@gmail.com>
2022-03-30 02:44:31 +0100

    'firstdeliberation'

---
+++ app/Models/Deliberation.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Deliberation extends Model
+{
+    use HasFactory;
+
+    public $fillable = [
+        'yearId',
+        'semesters',
+        'fieldId',
+        'groupId',
+        'authorId',
+        'participants',
+        'start',
+        'end',
+        'report',
+        'revealTicket',
+        'hideTicket', 
+    ];
+
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\GestionDeliberation\IndexController;
+use App\Http\Controllers\GestionDeliberation\EnregistrerDeliberationsController;
+use App\Http\Controllers\GestionDeliberation\DeliberationInfosController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -22,6 +26,20 @@ Auth::routes();
 Route::group(["prefix"=>"gestion_salle", "as"=>"gestion_salle."], function ()
 {
     Route::get('/', "GestionSalleDeClasse\SalleController@index")->name("index");
+});
+
+Route::group(["prefix"=>"gestion_deliberation", "as"=>"gestion_deliberation."], function ()
+{
+    
+    Route::middleware('auth')->group(function () {
+        Route::get('/', "GestionDeliberation\IndexController@index")->name("index");
+        Route::post('/search', "GestionDeliberation\IndexController@recherche")->name("search");
+        Route::get('/enregistrerDeliberation', "GestionDeliberation\EnregistrerDeliberationsController@create")->name('nouvelledeliberation');
+        Route::post('/enregistrerDeliberation', "GestionDeliberation\EnregistrerDeliberationsController@store")->name('enregistrerdeliberation');
+        Route::post('/delibinfos', "GestionDeliberation\DeliberationInfosController@index")->name('delibinfos');
+        Route::post('/ouvrir', "GestionDeliberation\DeliberationInfosController@show")->name('ouvrir');
+        Route::post('/voir', "GestionDeliberation\IndexController@show")->name('voir');
+    });
 });
 
 Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
