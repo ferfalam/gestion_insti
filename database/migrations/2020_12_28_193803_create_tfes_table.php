421a5727d848e85e6423153e0fa98262c39aae05
Ariel Carmen <makandjou3000@gmail.com>
2022-03-30 19:41:08 +0100

    toutes les branches ont été pushées

--- database/migrations/2020_12_28_193803_create_tfes_table.php
+++ database/migrations/2020_12_28_193803_create_tfes_table.php
@@ -14,9 +14,9 @@ class CreateTfesTable extends Migration
     public function up()
     {
         Schema::enableForeignKeyConstraints();
-        Schema::create('tves', function (Blueprint $table) {
+        Schema::create('tfes', function (Blueprint $table) {
             $table->id();
-            $table->foreign('document_id')->references('id')->on('documents')->onDelete('cascade');
+            $table->foreignId('document_id')->references('id')->on('documents')->onDelete('cascade');
             $table->string('theme');
             $table->unsignedBigInteger('user_id');
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
