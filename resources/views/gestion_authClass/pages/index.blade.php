066cb9efc99bae916d3a50bd8f6fc9ab6b51fb9a
gillesdmagi <dagilles84@gmail.com>
2022-05-05 13:24:36 +0200

    m

--- resources/views/gestion_authClass/pages/index.blade.php
+++ resources/views/gestion_authClass/pages/index.blade.php
@@ -8,6 +8,7 @@
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                     <div class="p-6 bg-white border-b border-gray-200">
                         <h2>Authentification et Classement des étudiants</h2>
+                        <h3></h3>
                     </div>
                 </div>
             </div>
